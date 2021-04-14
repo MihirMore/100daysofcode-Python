708c6beff4d139cf756c29ebf565cfae6d0d1905
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-04-14 15:35:43 +0530

    Create main2.py

---
+++ Day-1/day-1-4-exercise/main2.py
@@ -0,0 +1,18 @@
+# 🚨 Don't change the code below 👇
+a = int(input("a: "))
+b = int(input("b: "))
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+a = a + b
+b = a - b
+a = a - b
+
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a:",a)
+print("b:",b)
