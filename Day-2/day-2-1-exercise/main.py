31c2bfb3260b1e64be4db396b7722c17e1f59f9d
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-04-15 17:22:00 +0530

    Create main.py

---
+++ Day-2/day-2-1-exercise/main.py
@@ -0,0 +1,16 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+first_number = int(two_digit_number[0])
+second_number = int(two_digit_number[1])
+
+print(f"{first_number} + {second_number} = {first_number + second_number}")
+
+
+
+
+
+
