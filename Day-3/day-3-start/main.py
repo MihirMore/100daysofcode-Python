d8bc63d06a6196ad4c7285f32206e55a925deace
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-04-16 16:07:10 +0530

    Create main.py

---
+++ Day-3/day-3-start/main.py
@@ -0,0 +1,7 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if (height > 120):
+  print("Yaay! You can ride the rollercoaster.")
+else:
+  print("Sorry, you have to grow taller to ride the rollercoaster.")
