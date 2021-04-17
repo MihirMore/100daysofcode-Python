66bfa074fb60258982ce8f4da874ef8659a14697
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-04-17 18:14:34 +0530

    Create head_tails.py

---
+++ Day-4/day-4-1-exercise/head_tails.py
@@ -0,0 +1,9 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+import random
+
+result = random.randint(0 , 1)
+if (result == 1):
+  print("Heads.")
+else:
+  print("Tails.")
