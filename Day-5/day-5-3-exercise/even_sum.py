bc37c3cb91c9333467ae988da9fc3237b3500655
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-04-18 17:50:41 +0530

    Create even_sum.py

---
+++ Day-5/day-5-3-exercise/even_sum.py
@@ -0,0 +1,13 @@
+#Write your code below this row 👇
+sum = 0
+for i in range(2 , 101 , 2):
+  sum += i
+
+print(sum)  
+
+total = 0
+for j in range(1, 101):
+    if(j % 2 == 0):
+        total += j
+
+print(total)        
