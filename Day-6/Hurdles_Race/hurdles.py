d0f2e166ba6028a46f12530e6af0021a521851e8
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-04-19 16:51:14 +0530

    Create hurdles.py

---
+++ Day-6/Hurdles_Race/hurdles.py
@@ -0,0 +1,15 @@
+# Source-Code (https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json)
+#  def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# for i in range(0 ,6):
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
