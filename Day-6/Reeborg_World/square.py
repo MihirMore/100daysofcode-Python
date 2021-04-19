6f19003662f354d94eef58a63aeea879a158c4fc
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-04-19 17:31:56 +0530

    Update square.py

--- Day-6/Reeborg_World/square.py
+++ Day-6/Reeborg_World/square.py
@@ -1,4 +1,4 @@
-# [Source_Code](https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Alone&url=worlds%2Ftutorial_en%2Falone.json)
+#
 # def turn_right():
 #     turn_left()
 #     turn_left()
