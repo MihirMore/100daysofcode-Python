42f60064bcc35def3ddc7b13507cc3e98279fc35
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-04-19 17:50:15 +0530

    Create hurdle4.py

---
+++ Day-6/Final_Hurdle/hurdle4.py
@@ -0,0 +1,18 @@
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+# def jump():
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+# while not at_goal():
+#     if (wall_on_right() and wall_in_front()):
+#         turn_left()
+#     elif (wall_on_right()):
+#         move()        
+#     else:
+#         jump()
+    
+    
