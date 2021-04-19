afbd3150d200a36a10c12bc608f00e70b26cf72a
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-04-19 17:31:45 +0530

    Create hurdle3.py

---
+++ Day-6/Dynamic_hurdle/hurdle3.py
@@ -0,0 +1,18 @@
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+# def jump():
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+# while not at_goal():
+#     if (wall_in_front()):
+#         jump()
+#     elif (front_is_clear()):
+#         move()
+     
