f698d368c795d08c591ec83886aa82b252450ea0
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-04-20 13:57:17 +0530

    Create compare.py

---
+++ Day-7/day-7-hangman-1/compare.py
@@ -0,0 +1,21 @@
+#Step 1 
+
+word_list = ["aardvark", "baboon", "camel"]
+
+#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+
+import random
+chosen_word = random.choice(word_list)
+print(chosen_word)
+
+#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+
+guess = input("Please guess a letter: ")
+
+#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+
+for letter in chosen_word:
+    if letter == guess:
+        print("Right")
+    else:
+        print("Wrong")    
