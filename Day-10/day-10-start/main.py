0b14b40426efac0a7ebd9ce9844c196eb9977628
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-04-23 17:08:53 +0530

    Update main.py

--- Day-10/day-10-start/main.py
+++ Day-10/day-10-start/main.py
@@ -1,6 +1,11 @@
 def format_name(first_name, last_name):
+  # docstrings
+  """ Takes first and last name as arguments and returns 
+      formatted version in title case.
+  """
   if first_name == '' or last_name == '':
     return "Please enter first name or last name."
   return f"{first_name} {last_name}".title()
 
 print(format_name(input("What's your first name? "), input("What's your last name?")))
+
