32a7f76900aac79c9e734bd1e4f1270676b22e34
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-04-26 16:24:43 +0530

    Update main.py

--- Day-13/day-13-exercise-1/main.py
+++ Day-13/day-13-exercise-1/main.py
@@ -33,12 +33,12 @@ word_per_page = int(input("Number of words per page: "))
 total_words = pages * word_per_page
 print(total_words)
 
-# #Use a Debugger
-# def mutate(a_list):
-#   b_list = []
-#   for item in a_list:
-#     new_item = item * 2
-#   b_list.append(new_item)
-#   print(b_list)
+#Use a Debugger
+def mutate(a_list):
+  b_list = []
+  for item in a_list:
+    new_item = item * 2
+    b_list.append(new_item)
+  print(b_list)
 
-# mutate([1,2,3,5,8,13])
+mutate([1,2,3,5,8,13])
