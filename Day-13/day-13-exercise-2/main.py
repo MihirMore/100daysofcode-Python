1aba0fb80e863836678b123a93caccca6e634946
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-04-26 16:45:59 +0530

    Create main.py

---
+++ Day-13/day-13-exercise-2/main.py
@@ -0,0 +1,8 @@
+number = int(input("Which number do you want to check? "))
+
+# Error in the below line of code; Before if number % 2 = 0:
+if number % 2 == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
+  
