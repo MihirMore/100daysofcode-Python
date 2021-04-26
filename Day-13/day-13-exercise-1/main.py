3c1d6f9b14d9c5c636f092701e4df3511aaa19ce
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-04-26 16:14:50 +0530

    Update main.py

--- Day-13/day-13-exercise-1/main.py
+++ Day-13/day-13-exercise-1/main.py
@@ -7,14 +7,14 @@ def my_function():
       print("You got it")
 my_function()
 
-# # Reproduce the Bug
-# from random import randint
-# dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
-# dice_num = randint(1, 6)
-# print(dice_imgs[dice_num-1])
+# Reproduce the Bug
+from random import randint
+dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+dice_num = randint(1, 6)
+print(dice_imgs[dice_num-1])
 
 # # Play Computer
-# year = int(input("What's your year of birth?"))
+# year = int(input("What's your year of birth? "))
 # if year > 1980 and year < 1994:
 #   print("You are a millenial.")
 # elif year > 1994:
