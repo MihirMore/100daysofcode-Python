09fbf0d783c236f708dc0359a196ab2671ba6c4f
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-04-29 21:20:47 +0530

    Delete main.py

--- Day-16/day-16-exercise-2/main.py
+++
@@ -1,9 +0,0 @@
-from prettytable import PrettyTable
-
-table = PrettyTable()
-table.add_column("Pokemon Name", ["Pikachu" , "Squirtle" , "Charmander"])
-table.add_column("Type", ["Electric", "Water", "Fire"])
-table.align = "l"
-table.header = True
-table.border = True
-print(table)
