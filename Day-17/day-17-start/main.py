ed25b3cd5bf42012d3caacf6fa5b02aad79aa4b9
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-04-30 19:21:12 +0530

    Update main.py

--- Day-17/day-17-start/main.py
+++ Day-17/day-17-start/main.py
@@ -4,10 +4,20 @@ class User:
         self.id = user_id
         self.username = username
         self.followers = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1    
 
 user_1 = User("001","mihir")
+user_2 = User("002", "Raj")
 
+user_1.follow(user_2)
+print(user_1.followers)
+print(user_1.following)
+print(user_2.followers)
+print(user_2.following)
 
-print(user_1.username)
 
 
