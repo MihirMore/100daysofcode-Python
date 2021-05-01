7f4a7e54a3495eab969bb99639e1d4bd4963215a
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-05-02 00:34:55 +0530

    Update main.py

--- Day-18/day-18-exercise-1/main.py
+++ Day-18/day-18-exercise-1/main.py
@@ -7,16 +7,6 @@ for _ in range(4):
     tim.forward(150)
     tim.right(90)
 
-tim.pensize(width=3)
-for _ in range(20):
-    tim.pendown()
-    tim.forward(8)
-    tim.penup()
-    tim.forward(8)
-
-
-
-
 screen = Screen()
 screen.exitonclick()
 
