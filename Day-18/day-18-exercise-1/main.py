a4b42639d51677f8f94bd50b24527f98d592825c
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-05-01 19:55:13 +0530

    Create main.py

---
+++ Day-18/day-18-exercise-1/main.py
@@ -0,0 +1,11 @@
+from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle()
+timmy_the_turtle.shape(name="turtle")
+timmy_the_turtle.color("tomato3")
+timmy_the_turtle.forward(150)
+timmy_the_turtle.right(90)
+timmy_the_turtle.forward(150)
+
+screen = Screen()
+screen.exitonclick()
