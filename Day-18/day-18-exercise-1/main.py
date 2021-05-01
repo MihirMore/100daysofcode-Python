9e3f86b50341928bb35a30af9517ef7811ade39f
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-05-01 20:08:42 +0530

    Update main.py

--- Day-18/day-18-exercise-1/main.py
+++ Day-18/day-18-exercise-1/main.py
@@ -3,9 +3,9 @@ from turtle import Turtle, Screen
 timmy_the_turtle = Turtle()
 timmy_the_turtle.shape(name="turtle")
 timmy_the_turtle.color("tomato3")
-timmy_the_turtle.forward(150)
-timmy_the_turtle.right(90)
-timmy_the_turtle.forward(150)
+for _ in range(4):
+    timmy_the_turtle.forward(150)
+    timmy_the_turtle.right(90)
 
 screen = Screen()
 screen.exitonclick()
