710bfdf2358fe195ffc53e01e6658b293311cb27
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-05-02 19:34:23 +0530

    Update turtle_race.py

--- Day-19/Final-Project/turtle_race.py
+++ Day-19/Final-Project/turtle_race.py
@@ -1,9 +1,11 @@
 from turtle import Turtle, Screen
+import random
 
-
+is_race_on  = False
 screen = Screen()
 screen.setup(width=600, height=500)
 colors = ["red", "orange", "gold","green","blue", "purple"]
+all_turtles = []
 
 user_bet = screen.textinput(title="Make your bet", prompt="Which turtle is going to win the race? Enter a colour: ")
 print(user_bet)
@@ -11,12 +13,36 @@ print(user_bet)
 y_point = -160
 
 for item in range(0, 6):
-    tim = Turtle(shape="turtle")
-    tim.color(colors[item])
-    tim.penup()
-    tim.goto(x=-280, y=y_point)
-    tim.pendown()
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(colors[item])
+    new_turtle.penup()
+    new_turtle.goto(x=-280, y=y_point)
+    new_turtle.pendown()
+    all_turtles.append(new_turtle)
     y_point += 60
+
+print(all_turtles)
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+
+    for turtle in all_turtles:
+        if turtle.xcor() > 280:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won! The winning turtle is {winning_color}.")
+                turtle.write(f"You have won! The winner is {winning_color}", font=("Calibri", 16, "bold"))
+            else:
+                print(f"You've lost! The winning turtle is {winning_color}.") 
+                turtle.write(f"You have lost. The winner is {winning_color}", font=("Calibri", 16, "bold"))   
+
+    for turtle in all_turtles:
+        random_distance = random.randint(1, 10)
+        turtle.penup()
+        turtle.forward(random_distance)
 
 
 
