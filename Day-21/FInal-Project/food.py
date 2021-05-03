7c29db4f02731c1432af30173f620de52b824fe0
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-05-04 00:02:02 +0530

    Create food.py

---
+++ Day-21/FInal-Project/food.py
@@ -0,0 +1,18 @@
+from turtle import Turtle
+import random
+class Food(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.shapesize(stretch_len = 0.5, stretch_wid = 0.5)
+        self.color("firebrick")
+        self.speed("fastest")
+        self.refresh_food()
+
+        
+    def refresh_food(self):
+        random_x = random.randint(-285, 285)
+        random_y = random.randint(-285, 285)
+        self.goto(random_x, random_y)   
