7ca92e81bf26f1b5bf0647cd6fda657a437c071a
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-05-04 01:50:42 +0530

    Update food.py

--- Day-21/FInal-Project/food.py
+++ Day-21/FInal-Project/food.py
@@ -6,13 +6,13 @@ class Food(Turtle):
         super().__init__()
         self.shape("circle")
         self.penup()
-        self.shapesize(stretch_len = 0.4, stretch_wid = 0.4)
-        self.color("firebrick")
+        self.shapesize(stretch_len = 0.5, stretch_wid = 0.5)
+        self.color("chartreuse")
         self.speed("fastest")
         self.refresh_food()
 
         
     def refresh_food(self):
-        random_x = random.randint(-285, 285)
-        random_y = random.randint(-285, 285)
+        random_x = random.randint(-280, 280)
+        random_y = random.randint(-280, 280)
         self.goto(random_x, random_y)   
