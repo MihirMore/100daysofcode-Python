bec9f3e6fb66629f9a1150ad8843e0f77798fe61
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-05-03 12:47:09 +0530

    Update snake_game.py

--- Day-20/Final-Project/snake_game.py
+++ Day-20/Final-Project/snake_game.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -19,8 +20,15 @@ for position in starting_positions:
 
 game_is_on = True
 while game_is_on:
-    for seg in segments:
-        seg.forward(10)
+    screen.update()
+    time.sleep(.1)
+    
+    for seg_num in range(len(segments) - 1, 0, -1):
+        new_x = segments[seg_num - 1].xcor()
+        new_y = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(x=new_x, y=new_y)
+    
+
 
 
 
