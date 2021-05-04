f3adaad9bf4ade68c3fd9dbb95860693f33c633f
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-05-04 15:00:11 +0530

    Create main.py

---
+++ Day-20/slicing/main.py
@@ -0,0 +1,7 @@
+piano_keys = ["a", "b", "c", "d", "e", "f", "g"]
+piano_tuple = ("do", "re", "mi", "fa", "so", "la", "ti")
+
+print(piano_keys[2:5])
+print(piano_keys[::-1])
+
+print(piano_tuple[1:])
