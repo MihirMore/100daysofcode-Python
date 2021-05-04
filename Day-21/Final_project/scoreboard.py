9a2fbb66cd412d964050cb6b38635d5f8dcd68f8
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-05-04 18:58:12 +0530

    Update scoreboard.py

--- Day-21/Final_project/scoreboard.py
+++ Day-21/Final_project/scoreboard.py
@@ -9,7 +9,7 @@ class Scoreboard(Turtle):
         self.score = 0
         self.color("white")
         self.penup()
-        self.goto(0, 260)
+        self.goto(0, 240)
         self.update_scoreboard()
         self.hideturtle()
 
