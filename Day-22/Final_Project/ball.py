a9e2a3b068e34e3da6d29a0e2ac68112fb9e8391
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-05-05 17:04:13 +0530

    Update ball.py

--- Day-22/Final_Project/ball.py
+++ Day-22/Final_Project/ball.py
@@ -5,11 +5,12 @@ class Ball(Turtle):
     def __init__(self):
         super().__init__()
         self.shape("circle")
-        self.color("white")
+        self.color("gold1")
         # self.shapesize(0.7, 0.7)
         self.penup()
         self.x_move = 10
         self.y_move = 8
+        self.ball_speed = 0.08
                 
 
     def move(self):
@@ -19,9 +20,12 @@ class Ball(Turtle):
 
     def bounce_y(self):
         self.y_move *= -1
+        self.ball_speed *= 0.99
 
     def bounce_x(self):
         self.x_move *= -1  
         
     def reset_position(self):        
-        self.goto(0, 0)         
+        self.goto(0, 0)   
+        self.bounce_x()
+        self.ball_speed = 0.08      
