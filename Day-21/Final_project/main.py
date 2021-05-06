4727199fae307678c2300a111b2880f91afda6cc
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-05-06 18:09:39 +0530

    Update main.py

--- Day-21/Final_project/main.py
+++ Day-21/Final_project/main.py
@@ -36,14 +36,14 @@ while game_is_on:
 
     # Detect collision with wall
     if snake.head.xcor() > 310 or snake.head.xcor() < -310 or snake.head.ycor() > 290 or snake.head.ycor() < -275:
-        game_is_on = False
-        scoreboard.game_over() 
+        scoreboard.reset_score()
+        snake.reset_snake()
 
     # Detect collision with tail
     # If the head collides with any segemnt of the snake then end the game.
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset_score()
+            snake.reset_snake()
 
 screen.exitonclick()
