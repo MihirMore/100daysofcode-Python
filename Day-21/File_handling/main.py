e749c3a5c8183ad5232e43affda86f498a863ab3
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-05-06 18:11:34 +0530

    Update main.py

--- Day-21/File_handling/main.py
+++ Day-21/File_handling/main.py
@@ -1,4 +1,8 @@
-with open("my_file.txt") as fhand:
+# with open("my_file.txt") as fhand:
 
-    contents = fhand.read()
-    print(contents)
+#     contents = fhand.read()
+#     print(contents)
+
+
+with open("my_file.txt", mode = "a") as fhand:
+    fhand.write("Hello! I'm appending")
