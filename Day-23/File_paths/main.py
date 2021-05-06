91b1ed3cce5cee01c49d72f733f534b3a6e83604
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-05-06 21:15:03 +0530

    Create main.py

---
+++ Day-23/File_paths/main.py
@@ -0,0 +1,8 @@
+with open("..\\..\\..\\..\\..\\Desktop\\my_file.txt") as fhand:
+    contents = fhand.read()
+    print(contents)
+
+
+with open ("C:\\Users\\morem\\OneDrive\\Desktop\\my_file.txt") as file_1:
+    contents_1 = file_1.read()
+    print(contents_1)    
