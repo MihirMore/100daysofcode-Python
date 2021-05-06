cf95fb6f6734e99e4cc0c2c2ef3dd3f9f131b374
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-05-06 18:19:07 +0530

    Update scoreboard.py

--- Day-21/Final_project/scoreboard.py
+++ Day-21/Final_project/scoreboard.py
@@ -7,7 +7,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.highscore = 0
+        with open("data.txt") as data:
+            self.highscore = int(data.read())
 
         self.color("white")
         self.penup()
@@ -22,6 +23,8 @@ class Scoreboard(Turtle):
     def reset_score(self):
         if self.score > self.highscore:
             self.highscore = self.score
+            with open("data.txt", mode = "w") as data:
+                data.write(f"{self.highscore}")
         self.score = 0  
         self.update_scoreboard()  
 
