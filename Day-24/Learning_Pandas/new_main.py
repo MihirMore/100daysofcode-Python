1a9e2715964945128b9b4dc31590e45e216ca036
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-05-07 22:16:15 +0530

    Create new_main.py

---
+++ Day-24/Learning_Pandas/new_main.py
@@ -0,0 +1,13 @@
+import pandas
+
+data = pandas.read_csv("weather_data.csv")
+print(data["temp"])
+
+data_dict = data.to_dict()
+print(data_dict["temp"])
+
+data_list = data["temp"].to_list()
+print(data_list)
+print(sum(data_list) / len(data_list))
+
+print(data["temp"].max())
