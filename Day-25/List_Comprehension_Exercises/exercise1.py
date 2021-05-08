fc827d5ce4dc76ec8cd2156bd74e9ace2fa2f09f
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-05-08 23:12:52 +0530

    Update exercise1.py

--- Day-25/List_Comprehension_Exercises/exercise1.py
+++ Day-25/List_Comprehension_Exercises/exercise1.py
@@ -9,3 +9,7 @@ numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
 squared_numbers = [num*num for num in numbers]
 print(squared_numbers)
+#Alternate method
+new_numbers = [num**2 for num in numbers]
+print(new_numbers)
+
