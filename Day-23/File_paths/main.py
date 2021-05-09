62d06cc2ccfe4a3d64ea8ce10c0bbffac677ecea
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-05-09 21:14:29 +0530

    Files updated

--- Day-23/File_paths/main.py
+++
@@ -1,8 +0,0 @@
-with open("..\\..\\..\\..\\..\\Desktop\\my_file.txt") as fhand:
-    contents = fhand.read()
-    print(contents)
-
-
-with open ("C:\\Users\\morem\\OneDrive\\Desktop\\my_file.txt") as file_1:
-    contents_1 = file_1.read()
-    print(contents_1)    
