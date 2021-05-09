f10c733d9855d9cb2a52f2678903d9728cf8ab65
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-05-09 17:51:22 +0530

    Update main.py

--- Day-25/Dictionary_Comprehension_Exercise/Exercise-1/main.py
+++ Day-25/Dictionary_Comprehension_Exercise/Exercise-1/main.py
@@ -3,7 +3,7 @@ sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
 
 # Write your code below:
 
-result = {word:len(word)  for word in sentence.split() }
+result = {word:len(word) for word in sentence.split() }
 
 print(result)
 
