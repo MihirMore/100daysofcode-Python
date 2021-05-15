73d7542c8ed74254995e791293b05e9ae6038af1
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-05-15 18:30:40 +0530

    Update main.py

--- Day-30/Sandbox/main.py
+++ Day-30/Sandbox/main.py
@@ -13,10 +13,18 @@ except KeyError as error_message:
     print(f"The key {error_message} does not exist.")    
 else:
     content = file.read()
-    print(content)
-# Your own custom exceptions    
+    print(content)   
 finally:
     raise TypeError("This is an error I made up!")
 
+# Your custom exceptions 
 
+height = float(input("Height: "))
+weight = float(input("Weight: "))
+if height > 3:
+    raise ValueError("Human heights should not be over 3 meters")
+
+bmi = weight / height ** 2
+
+print(bmi)
 
