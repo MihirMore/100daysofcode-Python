929cc4bd1cd96230eefa9cedef49dfb6c81b46ba
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-05-15 20:55:01 +0530

    Update main.py

--- Day-29/Final-Project/Password_Manager/main.py
+++ Day-29/Final-Project/Password_Manager/main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from random import randint, choice, shuffle
 import pyperclip
+import json
 
 FONT_NAME = "COMIC SANS MS"
 
@@ -31,17 +32,30 @@ def save_data():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password
+        }
+    }
 
     if len(website) == 0 or len(password) == 0:
         messagebox.showwarning(title="Oops", message="Please don't leave any fields empty!")
-    else:
-        is_ok = messagebox.askokcancel(title=website,
-                                       message=f"These are the details you entered: \nEmail: {email} \nPassword: "
-                                               f"{password} \nIs it ok to save?")
-
-        if is_ok:
-            with open("data.txt", "a") as data_file:
-                data_file.write(f"{website} | {email} | {password} \n")
+    else:  
+        try:      
+            with open("data.json", "r") as data_file:
+                # Reading old data
+                data = json.load(data_file)
+        except FileNotFoundError:        
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)  
+        else:   
+            # Updating old data with new data  
+            data.update(new_data)
+            with open("data.json", "w") as data_file:
+                # Saving updated data
+                json.dump(data, data_file, indent=4)   
+        finally:
             website_entry.delete(0, END)
             password_entry.delete(0, END)
 
@@ -68,18 +82,21 @@ password_label = Label(text="Password :", font=(FONT_NAME, 12, "normal"))
 password_label.grid(column=0, row=3)
 
 # Creating entry to accept input
-website_entry = Entry(width=50)
-website_entry.grid(column=1, row=1, columnspan=2)
+website_entry = Entry(width=32)
+website_entry.grid(column=1, row=1)
 website_entry.focus()
 
 email_entry = Entry(width=50)
 email_entry.grid(column=1, row=2, columnspan=2)
-email_entry.insert(0, "mihirmore.18@gmail.com")
+email_entry.insert(0, "mihir@email.com")
 
 password_entry = Entry(width=32)
 password_entry.grid(column=1, row=3)
 
 # Create buttons
+search_password_button = Button(text="Search", bd=1, width=14)
+search_password_button.grid(column=2, row=1)
+
 generate_password_button = Button(text="Generate Password", bd=1, command=generate_password)
 generate_password_button.grid(column=2, row=3)
 
