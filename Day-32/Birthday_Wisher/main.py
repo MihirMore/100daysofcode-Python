729ada6ccaa43096ad727bccb294d38cabaea660
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-05-17 00:16:03 +0530

    Create main.py

---
+++ Day-32/Birthday_Wisher/main.py
@@ -0,0 +1,22 @@
+# import smtplib
+#
+# my_email = "testpythonsmtp123@gmail.com"
+# password = "Python@123"
+#
+# with smtplib.SMTP("smtp.gmail.com") as connection:
+#     connection.starttls()
+#     connection.login(user=my_email, password=password)
+#     connection.sendmail(from_addr=my_email, to_addrs="mihirmore.18@gmail.com", msg="Subject:Hello from python "
+#                                                                                    "project\n\n "
+#                                                                                    "This is a test email from python "
+#                                                                                    "project. Do not reply")
+#     connection.close()
+
+import datetime as dt
+
+now = dt.datetime.now()
+week = now.weekday()
+print(week)
+
+date_of_birth = dt.datetime(year=1995, month=12, day=15, hour=4)
+print(date_of_birth)
