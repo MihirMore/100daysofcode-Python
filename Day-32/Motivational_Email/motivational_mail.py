3e8bc7c171d3a1f1fc1bbde6b42a01ed183c5be7
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-05-17 17:41:39 +0530

    Create motivational_mail.py

---
+++ Day-32/Motivational_Email/motivational_mail.py
@@ -0,0 +1,19 @@
+import smtplib
+import datetime as dt
+import random
+
+MY_EMAIL = "youremail"
+PASSWORD = "yourpassword"
+
+now = dt.datetime.now()
+weekday = now.weekday()
+if weekday == 0:
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = random.choice(all_quotes)
+    print(quote)
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=MY_EMAIL, password=PASSWORD)
+        connection.sendmail(from_addr=MY_EMAIL, to_addrs=MY_EMAIL, msg=f"Subject: Monday Motivational Quote \n\n"
+                                                                       f"{quote}")
