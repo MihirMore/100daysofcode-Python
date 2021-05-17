bb3c4dbded6259f73c7d151b79f41888dd405aa1
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-05-17 17:41:34 +0530

    Create main.py

---
+++ Day-32/Motivational_Email/main.py
@@ -0,0 +1,21 @@
+import smtplib
+import datetime as dt
+
+my_email = "youremail"
+password = "yourpassword"
+
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=password)
+    connection.sendmail(from_addr=my_email, to_addrs="mihirmore.18@gmail.com", msg="Subject:Hello from python "
+                                                                                   "project\n\n "
+                                                                                   "This is a test email from python "
+                                                                                   "project. Do not reply")
+    connection.close()
+
+now = dt.datetime.now()
+week = now.weekday()
+print(week)
+
+date_of_birth = dt.datetime(year=1995, month=12, day=15, hour=4)
+print(date_of_birth)
