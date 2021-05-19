149bc46e57e6a53bc58410b9d63a36801d8167d6
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-05-20 01:24:43 +0530

    Update main.py

--- Day-33/ISS_Notifier/main.py
+++ Day-33/ISS_Notifier/main.py
@@ -51,3 +51,4 @@ while True:
                             to_addrs=MY_EMAIL,
                             msg="Subject:Look Up \n\nThe ISS is above you in the sky."
                             )
+                            
