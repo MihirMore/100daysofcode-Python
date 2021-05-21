a8082118e19b6d76acfc5d14ee30a708c6d2074a
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-05-21 23:59:17 +0530

    Update main.py

--- Day-35/Rain_Alert/main.py
+++ Day-35/Rain_Alert/main.py
@@ -5,7 +5,7 @@ API_KEY = "6554b41110709f2e4202dfe6a4d35e7d"
 parameters = {
     "lat": 19.075983,
     "lon": 72.877655,
-    "exclude": "minutely",
+    "exclude": "current,minutely,hourly",
     "appid": API_KEY
 }
 
@@ -13,4 +13,5 @@ response = requests.get(url="https://api.openweathermap.org/data/2.5/onecall", p
 print(response.status_code)
 response.raise_for_status()
 weather_data = response.json()
+print(weather_data)
 
