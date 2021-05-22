335904dc19369f3dfeb4dcda78a8021aba7f7309
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-05-22 14:39:58 +0530

    Update main.py

--- Day-35/Rain_Alert/main.py
+++ Day-35/Rain_Alert/main.py
@@ -2,13 +2,13 @@ import requests
 from twilio.rest import Client
 
 OWM_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
-API_KEY = "6554b41110709f2e4202dfe6a4d35e7d"
-account_sid = "AC446baf82e5cca3dbae630af5116cd96a"
-auth_token = "ad9ddf0446645abac71122180526b9e8"
+API_KEY = "OWM_API_KEY"
+account_sid = "Twilio account id"
+auth_token = "Twilio auth token"
 
 parameters = {
-    "lat": 46.811131,
-    "lon": -101.832603,
+    "lat": 46.8150051,
+    "lon": -101.829877,
     "exclude": "current,minutely,daily",
     "appid": API_KEY,
     "units": "metric"
@@ -23,6 +23,7 @@ will_rain = False
 
 weather_slice = weather_data["hourly"][:11]
 weather_id = weather_slice[0]["weather"][0]["id"]
+print(weather_id)
 
 for hour_data in weather_slice:
     if hour_data["weather"][0]["id"] < 700:
@@ -33,7 +34,7 @@ if will_rain:
     message = client.messages \
         .create(
         body=f"It's going to rain today. Remember to bring an umbrella. Weather id: {weather_id}",
-        from_='+16503977270',
-        to='+91 90824 90234'
+        from_='twilio_number',
+        to='my_number'
     )
     print(message.status)
