421acec2db6de0c1a9542576a1473568260a6293
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-05-24 16:29:53 +0530

    Update main.py

--- Day-37/Habit_Tracker/main.py
+++ Day-37/Habit_Tracker/main.py
@@ -1,14 +1,43 @@
 import requests
 
 PIXELA_ENDPOINT = "https://pixe.la/v1/users"
-PIXELA_TOKEN = "hwbehcvabefb&#$%^janwd"
+PIXELA_TOKEN = "your_token"
+USERNAME = "your_username"
 
 user_params = {
     "token": PIXELA_TOKEN,
-    "username": "mihirqd6345",
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
 }
+# API call to create user
+user_response = requests.post(url=PIXELA_ENDPOINT, json=user_params)
+print(user_response.text)
 
-response = requests.post(url=PIXELA_ENDPOINT, json=user_params)
+# API call to create graph for habit
+GRAPH_ENDPOINT = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": "graph1",
+    "name": "Reading Graph",
+    "unit": "pages",
+    "type": "int",
+    "color": "ajisai",
+}
+
+headers = {
+    "X-USER-TOKEN": PIXELA_TOKEN
+}
+
+response = requests.post(url=GRAPH_ENDPOINT, json=graph_config, headers=headers)
+print(response.text)
+
+UPDATE_GRAPH_ENDPOINT = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs/graph1"
+
+update_pixel = {
+    "date": "20210524",
+    "quantity": "300"
+}
+
+response = requests.post(url=UPDATE_GRAPH_ENDPOINT, json=update_pixel, headers=headers)
 print(response.text)
