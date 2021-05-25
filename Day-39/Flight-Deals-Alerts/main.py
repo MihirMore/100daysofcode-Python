149593715b3497268bfe15150f192cf171098542
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-05-25 15:42:25 +0530

    Update main.py

--- Day-39/Flight-Deals-Alerts/main.py
+++ Day-39/Flight-Deals-Alerts/main.py
@@ -1,11 +1,18 @@
 from data_manager import DataManager
+from flight_search import FlightSearch
+from datetime import datetime, timedelta
+from notification_manager import NotificationManager
 
 data_manager = DataManager()
 sheet_data = data_manager.get_destination_data()
-print(sheet_data)
+flight_search = FlightSearch()
+notification_manager = NotificationManager()
+
+ORIGIN_CITY_IATA = "BOM"
 
 if sheet_data[0]["iataCode"] == "":
     from flight_search import FlightSearch
+
     flightsearch = FlightSearch()
     for row in sheet_data:
         row["iataCode"] = flightsearch.get_destination_code(city_name=row["city"])
@@ -13,3 +20,15 @@ if sheet_data[0]["iataCode"] == "":
 
     data_manager.destination_data = sheet_data
     data_manager.update_destination_code()
+
+tomorrow = datetime.now() + timedelta(days=1)
+six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
+
+for destination in sheet_data:
+    flight = flight_search.check_flights(ORIGIN_CITY_IATA, destination["iataCode"], from_time=tomorrow, to_time= six_month_from_today)
+    if flight is not None:
+        print(flight.price)
+        if flight.price < destination["lowestPrice"]:
+            notification_manager.send_sms(
+                message=f"Low price alert! Only ₹{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to {flight.destination_city}-{flight.destination_airport}, from {flight.out_date} to {flight.return_date}. "
+            )
