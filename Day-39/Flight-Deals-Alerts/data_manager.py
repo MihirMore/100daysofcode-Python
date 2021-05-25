7a337f545214c12940da4b0a3a4059a853fe7c16
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-05-25 13:01:11 +0530

    Create data_manager.py

---
+++ Day-39/Flight-Deals-Alerts/data_manager.py
@@ -0,0 +1,29 @@
+import requests
+from pprint import pprint
+
+SHEETY_PRICES_ENDPOINT = "YOUR_ENDPOINT"
+
+
+class DataManager:
+    def __init__(self):
+        self.destination_data = {}
+
+    def get_destination_data(self):
+        response = requests.get(url=SHEETY_PRICES_ENDPOINT)
+        data = response.json()
+        pprint(data)
+        self.destination_data = data["prices"]
+        return self.destination_data
+
+    def update_destination_code(self):
+        for city in self.destination_data:
+            new_data = {
+                "price": {
+                    "iataCode": city["iataCode"]
+                }
+            }
+            response = requests.put(
+                url=f"{SHEETY_PRICES_ENDPOINT}/{city['id']}",
+                json=new_data
+            )
+            print(response.text)
