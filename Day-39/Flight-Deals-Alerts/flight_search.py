8e4efbdd710a12da4013b33ce14646871587184a
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-05-25 13:01:42 +0530

    Create flight_search.py

---
+++ Day-39/Flight-Deals-Alerts/flight_search.py
@@ -0,0 +1,11 @@
+
+TEQUILA_ENDPOINT = "https://tequila-api.kiwi.com"
+TEQUILA_API_KEY = "YOUR_API_KEY"
+
+
+class FlightSearch:
+
+    def get_destination_code(self, city_name):
+        code = "TESTING"
+        return code
+
