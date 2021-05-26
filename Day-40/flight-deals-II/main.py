254874865dc57258b0d32354505e4858c99545f6
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-05-26 23:33:56 +0530

    Update main.py

--- Day-40/flight-deals-II/main.py
+++ Day-40/flight-deals-II/main.py
@@ -8,7 +8,7 @@ sheet_data = data_manager.get_destination_data()
 flight_search = FlightSearch()
 notification_manager = NotificationManager()
 
-ORIGIN_CITY_IATA = "LON"
+ORIGIN_CITY_IATA = "BOM"
 
 if sheet_data[0]["iataCode"] == "":
     for row in sheet_data:
@@ -26,7 +26,19 @@ for destination in sheet_data:
         from_time=tomorrow,
         to_time=six_month_from_today
     )
+
+    if flight is None:
+        continue
+
     if flight.price < destination["lowestPrice"]:
-        notification_manager.send_sms(
-            message=f"Low price alert! Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to {flight.destination_city}-{flight.destination_airport}, from {flight.out_date} to {flight.return_date}."
-        )
+        message=f"Low price alert! Only ₹ {flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to " \
+                f"{flight.destination_city}-{flight.destination_airport}, from {flight.out_date} to {flight.return_date}."
+
+        if flight.stop_overs > 0:
+            message += f"\nFlight has {flight.stop_overs} stop over, via {flight.via_city}."
+            print(message)
+
+        link = f"https://www.google.co.uk/flights?hl=en#flt={flight.origin_airport}.{flight.destination_airport}." \
+               f"{flight.out_date}*{flight.destination_airport}.{flight.origin_airport}.{flight.return_date}"
+
+        notification_manager.send_sms(message)
