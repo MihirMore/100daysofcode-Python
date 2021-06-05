55a1ebe13876faa841a8aab089450eb5a052cbc4
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-06-05 22:02:18 +0530

    Update main.py

---
+++ Day-45/Beautiful-Soup/main.py
@@ -0,0 +1,7 @@
+from bs4 import BeautifulSoup
+
+with open("website.html", encoding="utf8") as file:
+    contents = file.read()
+
+soup = BeautifulSoup(contents, "html.parser")
+print(soup.title.string)
