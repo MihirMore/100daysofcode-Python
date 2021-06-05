64e81dd6df01e0bb3b721e379a21b01cfff6bf53
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-06-05 22:52:39 +0530

    Update main.py

--- Day-45/Beautiful-Soup/main.py
+++ Day-45/Beautiful-Soup/main.py
@@ -17,3 +17,9 @@ for tag in anchor_tags:
 
 heading = soup.find(name="h1", id="name")
 print(heading)
+
+section_heading = soup.find(name="h3", class_="heading")
+print(section_heading.string)
+
+company_url = soup.select_one(selector="p a")
+print(company_url)
