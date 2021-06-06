6e0b2928273f59c71f3f432989ac992a8a92055c
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-06-06 16:59:29 +0530

    Update website_scraping.py

--- Day-45/Live-Website-Data/website_scraping.py
+++ Day-45/Live-Website-Data/website_scraping.py
@@ -1,1 +1,13 @@
 from bs4 import BeautifulSoup
+import requests
+
+response = requests.get("https://news.ycombinator.com/")
+yc_news_page = response.text
+
+soup = BeautifulSoup(yc_news_page, "html.parser")
+article_tag = soup.find(name="a", class_="storylink")
+article_text = article_tag.get_text()
+article_link = article_tag.get("href")
+print(article_link)
+article_upvote = soup.find(name="span", class_="score").get_text()
+print(article_upvote)
