6a09ad5d5c6aac1feee86950f835fa8372b3f6f1
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-06-07 21:38:43 +0530

    Update main.py

--- Day-48/Selenium_Test/main.py
+++ Day-48/Selenium_Test/main.py
@@ -6,5 +6,13 @@ driver = webdriver.Chrome(executable_path=chrome_driver_path)
 driver.get("https://www.python.org/")
 search_bar = driver.find_element_by_name("q")
 print(search_bar.tag_name)
+print(search_bar.get_attribute("placeholder"))
+
+logo = driver.find_element_by_class_name("python-logo")
+print(logo.size)
+
+documentation_link = driver.find_element_by_css_selector(".documentation-widget a")
+print(documentation_link.text)
+
 
 driver.quit()
