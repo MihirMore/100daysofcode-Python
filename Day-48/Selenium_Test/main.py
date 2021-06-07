af34a858e4c2861e525b2a5e896ceba8b616d8ee
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-06-07 19:39:42 +0530

    Update main.py

--- Day-48/Selenium_Test/main.py
+++ Day-48/Selenium_Test/main.py
@@ -4,3 +4,5 @@ chrome_driver_path = "C:\Program Files\chromedriver_win32\chromedriver.exe"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
 driver.get("https://www.youtube.com")
+
+driver.quit()
