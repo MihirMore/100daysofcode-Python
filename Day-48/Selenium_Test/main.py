da681997ff796e46c72c30df1c8e8228b6819b3f
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-06-07 19:51:18 +0530

    Update main.py

--- Day-48/Selenium_Test/main.py
+++ Day-48/Selenium_Test/main.py
@@ -3,8 +3,8 @@ from selenium import webdriver
 chrome_driver_path = "C:\Program Files\chromedriver_win32\chromedriver.exe"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
-driver.get("https://www.amazon.in/JBL-Waterproof-Bluetooth-Speaker-PartyBoost/dp/B07SVH63PX/ref=sr_1_2_sspa?dchild=1&keywords=jbl+speaker&qid=1623075045&sr=8-2-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUFON1E4WU1UVzhORlQmZW5jcnlwdGVkSWQ9QTAxMTQ2MTU0VDZDSVZIWjVOV0QmZW5jcnlwdGVkQWRJZD1BMDE0NjgzMTI5TUhMMTlFTUdMUlEmd2lkZ2V0TmFtZT1zcF9hdGYmYWN0aW9uPWNsaWNrUmVkaXJlY3QmZG9Ob3RMb2dDbGljaz10cnVl")
-price = driver.find_element_by_id("priceblock_ourprice")
-print(price.text)
+driver.get("https://www.python.org/")
+search_bar = driver.find_element_by_name("q")
+print(search_bar.tag_name)
 
 driver.quit()
