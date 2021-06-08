7ff320a801b1714ffbc20d99e8187f3a6e003243
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-06-08 11:04:30 +0530

    Update interaction.py

--- Day-48/Wiki_Interaction/interaction.py
+++ Day-48/Wiki_Interaction/interaction.py
@@ -4,3 +4,7 @@ chrome_driver_path = "C:\Program Files\chromedriver_win32\chromedriver.exe"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
 driver.get("https://en.wikipedia.org/wiki/Main_Page")
+number_of_articles = driver.find_element_by_css_selector("#articlecount a")
+print(number_of_articles.text)
+
+driver.quit()
