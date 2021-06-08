cfc7404b4fb9e1c64ce3620ff74a517248cc0826
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-06-08 23:52:52 +0530

    Update interaction.py

--- Day-48/Wiki_Interaction/interaction.py
+++ Day-48/Wiki_Interaction/interaction.py
@@ -5,16 +5,16 @@ chrome_driver_path = "C:\Program Files\chromedriver_win32\chromedriver.exe"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
 driver.get("http://secure-retreat-92358.herokuapp.com/")
-# number_of_articles = driver.find_element_by_css_selector("#articlecount a")
-# print(number_of_articles)
-# number_of_articles.click()
+number_of_articles = driver.find_element_by_css_selector("#articlecount a")
+print(number_of_articles)
+number_of_articles.click()
 
-# all_portals = driver.find_element_by_partial_link_text("All portals")
-# all_portals.click()
+all_portals = driver.find_element_by_partial_link_text("All portals")
+all_portals.click()
 
-# search = driver.find_element_by_name("search")
-# search.send_keys("Python")
-# search.send_keys(Keys.ENTER)
+search = driver.find_element_by_name("search")
+search.send_keys("Python")
+search.send_keys(Keys.ENTER)
 
 f_name = driver.find_element_by_name("fName")
 f_name.send_keys("Mihir")
