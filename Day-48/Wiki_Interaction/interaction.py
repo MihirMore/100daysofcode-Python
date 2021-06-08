6909b4ea8cc7a081bc0ec227fe8db2aedb534b36
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-06-08 11:53:17 +0530

    Update interaction.py

--- Day-48/Wiki_Interaction/interaction.py
+++ Day-48/Wiki_Interaction/interaction.py
@@ -6,7 +6,8 @@ driver = webdriver.Chrome(executable_path=chrome_driver_path)
 driver.get("https://en.wikipedia.org/wiki/Main_Page")
 number_of_articles = driver.find_element_by_css_selector("#articlecount a")
 print(number_of_articles)
+# number_of_articles.click()
 
-number_of_articles.click()
-
+all_portals = driver.find_element_by_partial_link_text("All portals")
+all_portals.click()
 
