4de6fa254c5ded8d686588c549bbf4ae10a90db6
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-06-10 22:32:16 +0530

    Update linkedin_job_application.py

--- Day-49/Automated_Job_Application/linkedin_job_application.py
+++ Day-49/Automated_Job_Application/linkedin_job_application.py
@@ -3,8 +3,8 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 URL = "https://www.linkedin.com/jobs/search/?currentJobId=2576408054&f_AL=true&keywords=python%20developer"
-ACCOUNT_EMAIL = "YOUR EMAIL"
-ACCOUNT_PASSWORD = "YOUR PASSWORD"
+ACCOUNT_EMAIL = "YOUR_EMAIL"
+ACCOUNT_PASSWORD = "YOUR_ACCOUNT"
 
 chrome_driver_path = "C:\Program Files\chromedriver_win32\chromedriver.exe"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
@@ -15,7 +15,7 @@ driver.maximize_window()
 sign_in_button = driver.find_element_by_link_text("Sign in")
 sign_in_button.click()
 
-time.sleep(3)
+time.sleep(4)
 
 email_field = driver.find_element_by_id("username")
 email_field.send_keys(ACCOUNT_EMAIL)
@@ -23,7 +23,8 @@ password_field = driver.find_element_by_id("password")
 password_field.send_keys(ACCOUNT_PASSWORD)
 password_field.send_keys(Keys.ENTER)
 
-time.sleep(5)
-save_button = driver.find_element_by_xpath("/html/body/div[5]/div[3]/div[3]/div/div/section[2]/div/div/div[1]/div/div[1]/div/div[2]/div[3]/div/span/button")
+time.sleep(3)
+save_button = driver.find_element_by_xpath("/html/body/div[6]/div[3]/div[3]/div/div/section[2]/div/div/div["
+                                           "1]/div/div[1]/div/div[2]/div[3]/div/span/button")
 save_button.send_keys(Keys.ENTER)
-
+print("Job Saved")
