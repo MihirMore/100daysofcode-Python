0777f6145c3bf336105bcbf78bada30059c59399
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-06-13 23:03:02 +0530

    Update main.py

--- Day-50/Auto_Tinder_Swiping/main.py
+++ Day-50/Auto_Tinder_Swiping/main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import ElementClickInterceptedException, NoSuchElementException
 from time import sleep
 
 FB_EMAIL = "YOUR FACEBOOK LOGIN EMAIL"
@@ -37,3 +38,42 @@ password.send_keys(Keys.ENTER)
 # Switch back to Tinder window
 driver.switch_to.window(base_window)
 print(driver.title)
+
+# Delay by 5 seconds to allow page to load.
+sleep(5)
+
+# Allow location
+allow_location_button = driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')
+allow_location_button.click()
+
+# Disallow notifications
+notifications_button = driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[2]')
+notifications_button.click()
+
+# Allow cookies
+cookies = driver.find_element_by_xpath('//*[@id="content"]/div/div[2]/div/div/div[1]/button')
+cookies.click()
+
+# Tinder free tier only allows 100 "Likes" per day. If you have a premium account, feel free to change to a while loop.
+for n in range(100):
+
+    # Add a 1 second delay between likes.
+    sleep(1)
+
+    try:
+        print("called")
+        like_button = driver.find_element_by_xpath(
+            '//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/div[4]/button')
+        like_button.click()
+
+    # Catches the cases where there is a "Matched" pop-up in front of the "Like" button:
+    except ElementClickInterceptedException:
+        try:
+            match_popup = driver.find_element_by_css_selector(".itsAMatch a")
+            match_popup.click()
+
+        # Catches the cases where the "Like" button has not yet loaded, so wait 2 seconds before retrying.
+        except NoSuchElementException:
+            sleep(2)
+
+driver.quit()
