bdd89ec28bda22d90d0a7d55143fe79a73771059
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-06-14 16:18:47 +0530

    Update ISTCB.py

--- Day-51/Internet Speed Twitter Complaint Bot/ISTCB.py
+++ Day-51/Internet Speed Twitter Complaint Bot/ISTCB.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import time
 
 PROMISED_DOWN = 30
@@ -30,9 +31,21 @@ class InternetSpeedTwitterBot:
         up_speed = self.driver.find_element_by_xpath("/html/body/div[3]/div/div[3]/div/div/div/div[2]/div[3]/div["
                                                      "3]/div/div[3]/div/div/div[2]/div[1]/div[3]/div/div[2]/span")
         print(up_speed.text)
-        
+
     def tweet_at_provider(self):
-        pass
+        self.driver.get("https://twitter.com/login")
+        time.sleep(2)
+
+        email = self.driver.find_element_by_xpath("/html/body/div/div/div/div[2]/main/div/div/div[2]/form/div/div["
+                                                  "1]/label/div/div[2]/div/input")
+        password = self.driver.find_element_by_xpath("/html/body/div/div/div/div[2]/main/div/div/div[2]/form/div/div["
+                                                     "2]/label/div/div[2]/div/input")
+
+        email.send_keys(TWITTER_EMAIL)
+        password.send_keys(TWITTER_PASSWORD)
+        login_button = self.driver.find_element_by_xpath("/html/body/div/div/div/div[2]/main/div/div/div["
+                                                         "2]/form/div/div[3]/div/div/span/span")
+        login_button.send_keys(Keys.ENTER)
 
 
 bot = InternetSpeedTwitterBot(CHROME_DRIVER_PATH)
