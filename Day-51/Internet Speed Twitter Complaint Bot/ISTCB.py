043dba1e8161561f9a242a84da87ba81bba78409
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-06-14 15:59:51 +0530

    Update ISTCB.py

--- Day-51/Internet Speed Twitter Complaint Bot/ISTCB.py
+++ Day-51/Internet Speed Twitter Complaint Bot/ISTCB.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+import time
 
 PROMISED_DOWN = 30
 PROMISED_UP = 20
@@ -16,6 +17,20 @@ class InternetSpeedTwitterBot:
     def get_internet_speed(self):
         self.driver.get("https://www.speedtest.net/")
 
+        time.sleep(1)
+
+        go_button = self.driver.find_element_by_xpath("/html/body/div[3]/div/div[3]/div/div/div/div[2]/div[3]/div["
+                                                      "1]/a/span[4]")
+        go_button.click()
+        time.sleep(40)
+
+        down_speed = self.driver.find_element_by_xpath("/html/body/div[3]/div/div[3]/div/div/div/div[2]/div[3]/div["
+                                                       "3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span")
+        print(down_speed.text)
+        up_speed = self.driver.find_element_by_xpath("/html/body/div[3]/div/div[3]/div/div/div/div[2]/div[3]/div["
+                                                     "3]/div/div[3]/div/div/div[2]/div[1]/div[3]/div/div[2]/span")
+        print(up_speed.text)
+        
     def tweet_at_provider(self):
         pass
 
