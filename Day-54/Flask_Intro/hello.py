f32fedc0745b96215e55c6d99bd4196a599fff96
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-06-21 22:21:36 +0530

    Update hello.py

--- Day-54/Flask_Intro/hello.py
+++ Day-54/Flask_Intro/hello.py
@@ -2,7 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
+print(__name__)
+
 
 @app.route('/')
 def hello_world():
     return "Hello World!"
+
+
+if __name__ == "__main__":
+    app.run()
