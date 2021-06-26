cab801e446b0d8e1a340fdb67490a49e24135b99
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-06-26 17:23:24 +0530

    Update python_decorators.py

--- Day-54/Flask_Intro/python_decorators.py
+++ Day-54/Flask_Intro/python_decorators.py
@@ -12,3 +12,9 @@ def multiply(n1, n2):
 
 def divide(n1, n2):
     return n1 / n2
+
+
+# First-class objects, can be passed around as arguments e.g. int/string/float
+
+def calculate(calc_function, n1, n2):
+    calc_function(n1, n2)
