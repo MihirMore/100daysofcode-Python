cfdf0635b84892cb74f596137a0a08f13ef983f3
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-06-29 13:30:25 +0530

    Update main.py

--- Day-54/Flask_Intro/main.py
+++ Day-54/Flask_Intro/main.py
@@ -7,6 +7,7 @@ def delay_decorator(function):
         time.sleep(2)
         function()
         function()
+
     return wrapper_function()
 
 
@@ -15,3 +16,11 @@ def say_hello():
     print("Hello")
 
 
+@delay_decorator
+def say_bye():
+    print("Bye")
+
+
+@delay_decorator
+def say_greeting():
+    print("How are you?")
