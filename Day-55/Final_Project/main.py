59755c718576c68f2277bf28a2aa373f54208cdf
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-07-05 18:43:33 +0530

    Update main.py

--- Day-55/Final_Project/main.py
+++ Day-55/Final_Project/main.py
@@ -19,10 +19,5 @@ def greet(username, number):
     return f"Hello {user_name}!, you're {number} years old."
 
 
-@app.route('/bye')
-def bye():
-    print("Bye!")
-
-
 if __name__ == "__main__":
     app.run(debug=True)
