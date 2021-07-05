d0398a4c081f7ad33dfc02447ff6703fe15ddf0b
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-07-05 18:55:11 +0530

    Update main.py

--- Day-55/Final_Project/main.py
+++ Day-55/Final_Project/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello World!"
+    return '<h1 style="text-align: center">Hello World!</h1><p>This is a paragraph</p>'
 
 
 @app.route('/bye')
