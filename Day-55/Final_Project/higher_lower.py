73dfa58ef90eae4c2ad39f9166cdfcefb0600c2e
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-07-08 21:12:21 +0530

    README.md updated

--- Day-55/Final_Project/higher_lower.py
+++ Day-55/Final_Project/higher_lower.py
@@ -27,6 +27,6 @@ def guess_number(guess):
         return "<h1 style='color: green'>You found me!</h1>" \
                "<img src='https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif'/>"
 
-    
+
 if __name__ == "__main__":
     app.run(debug=True)
