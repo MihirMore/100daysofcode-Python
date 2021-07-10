8c0446fb18acba7ba704502e2fa73f6822b844ea
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-07-10 19:31:31 +0530

    Update exercise.py

--- Day-55/Advanced_Python_Decorator/exercise.py
+++ Day-55/Advanced_Python_Decorator/exercise.py
@@ -1,3 +1,6 @@
 def logging_decorator(fn):
     def wrapper(*args, **kwargs):
         print(f"You called {fn.__name__}{args}")
+        result = fn(args[0], args[1], args[2])
+        print(f"It returned {result}")
+    return wrapper
