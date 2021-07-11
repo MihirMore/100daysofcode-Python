156c653e10f65356f46053c679128b48798d5fe7
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-07-11 14:12:04 +0530

    Update higher_lower.py

--- Day-55/Final_Project/higher_lower.py
+++ Day-55/Final_Project/higher_lower.py
@@ -23,6 +23,10 @@ def guess_number(guess):
         return "<h1 style='color: red'>Too low, try again!</h1>"\
                "<img src='https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif'/>"
 
+    else:
+        return "<h1 style='color: green'>You found me!</h1>" \
+               "<img src='https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif'/>"
 
+    
 if __name__ == "__main__":
     app.run(debug=True)
