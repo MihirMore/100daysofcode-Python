f4bd92b43379d70d9b87ba53bfeae08d9cfd49b4
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-07-11 13:03:10 +0530

    Update higher_lower.py

--- Day-55/Final_Project/higher_lower.py
+++ Day-55/Final_Project/higher_lower.py
@@ -10,7 +10,8 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "<h1>Guess a number between 0 and 9</h1>" \
-        "<img src = 'https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif'/>"
+           "<img src = 'https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif'/>"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
